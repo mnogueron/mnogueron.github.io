dfcedef61c969c514e03a8e52ba1f9db4f5ea147
mnogueron <matthieu.nogueron@live.fr>
2018-05-11 11:30:59 +0200

    Fix webpack and eslint / add github link to page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+})
+
+function WrappedApp () {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <App/>
+    </MuiThemeProvider>
+  )
+}
+
+ReactDOM.render(<WrappedApp/>, document.getElementById('root'))
 registerServiceWorker()
