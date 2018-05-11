831cc6c6a3b0dc50fc65fbf291c672adbbc13f2e
mnogueron <matthieu.nogueron@live.fr>
2018-05-11 10:42:02 +0200

    Add header bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { withStyles } from 'material-ui/styles'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import IconButton from 'material-ui/IconButton'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+}
 
 class App extends Component {
+
+  _handleGithubIconPress = () => {
+    window.location = 'https://github.com/mnogueron'
+  }
+
   render() {
+    const { classes } = this.props
     return (
-      <div className="App">
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              Title
+            </Typography>
+
+            <IconButton
+              onClick={this._handleGithubIconPress}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Toolbar>
+
+        </AppBar>
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -18,4 +55,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default withStyles(styles)(App)
