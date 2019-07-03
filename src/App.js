ebb116fa55617f3c0e65196abd64480b8d2d8cb1
mnogueron <matthieu.nogueron@live.fr>
2019-07-04 00:46:20 +0200

    Cleanup app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React, { useRef } from 'react'
 import { makeStyles } from '@material-ui/styles'
 import Header from './components/Header'
 import PageContent from './components/PageContent'
-/*import AppBar from '@material-ui/core/AppBar'
-import Content from './Content'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import GitHubIcon from './assets/GitHubIcon'*/
 
 const useStyles = makeStyles({
   root: {
@@ -24,10 +18,6 @@ const App = (props) => {
   const classes = useStyles(props)
   const contentRef = useRef(null)
 
-  function handleGithubIconPress() {
-    window.location = 'https://github.com/mnogueron'
-  }
-
   function onExpandClick() {
     scrollToRef(contentRef)
   }
@@ -36,23 +26,6 @@ const App = (props) => {
     <div className={classes.root}>
       <Header onExpandClick={onExpandClick} />
       <PageContent ref={contentRef} />
-      {/*<AppBar position="absolute">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" className={classes.flex}>
-            mnogueron.github.io
-          </Typography>
-
-          <IconButton
-            onClick={handleGithubIconPress}
-            color="inherit"
-          >
-            <GitHubIcon/>
-          </IconButton>
-        </Toolbar>
-
-      </AppBar>*/}
-
-      {/*<Content/>*/}
     </div>
   )
 
