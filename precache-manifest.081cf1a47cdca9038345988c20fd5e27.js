aa2c395b128cecfc2eac0a2a2730f9a46e54dd34
mnogueron <matthieu.nogueron@live.fr>
2019-07-06 23:50:59 +0200

    Updates

---
+++ precache-manifest.081cf1a47cdca9038345988c20fd5e27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed1a79bff128d1b8fcceaed852d4778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaf4b9fd2874291cf3eb",
+    "url": "/static/css/main.ce22991c.chunk.css"
+  },
+  {
+    "revision": "663ba8d465ad99e38bb7",
+    "url": "/static/js/2.5a3c7d9e.chunk.js"
+  },
+  {
+    "revision": "aaf4b9fd2874291cf3eb",
+    "url": "/static/js/main.a7bc170b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9c2339d0eff692c266ae759649c9419",
+    "url": "/static/media/philippe-toupet-unsplash.f9c2339d.jpg"
+  },
+  {
+    "revision": "b75af53183e42b187a83c8b800b67511",
+    "url": "/static/media/simpletag.b75af531.png"
+  },
+  {
+    "revision": "2ce7f5bef237891a7c6782e1a72817a2",
+    "url": "/static/media/the-mist.2ce7f5be.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98788ed0a2f81f3760db963371d58fac.js"
+  "/precache-manifest.081cf1a47cdca9038345988c20fd5e27.js"
 );
 
 self.addEventListener('message', (event) => {
