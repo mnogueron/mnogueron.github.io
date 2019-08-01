ca0a49f2d3d20308a14c741de754c397c62a71e9
mnogueron <matthieu.nogueron@live.fr>
2019-08-01 13:28:39 +0200

    Add link to React and MUI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles'
 import grey from '@material-ui/core/colors/grey'
+import Link from '@material-ui/core/Link'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -17,7 +18,26 @@ const Footer = (props) => {
   const classes = useStyles(props)
   return (
     <div className={classes.root}>
-      Copyright © {year} All rights reserved | Portfolio designed and implemented by Matthieu Nogueron with React and Material-UI
+      {`Copyright © ${year} All rights reserved | Portfolio designed and implemented by Matthieu Nogueron with `}
+      <Link
+        href={'https://reactjs.org'}
+        target="_blank"
+        rel="noopener noreferrer"
+        underline={'always'}
+        color="inherit"
+      >
+        {'React'}
+      </Link>
+      {' and '}
+      <Link
+        href={'https://material-ui.com'}
+        target="_blank"
+        rel="noopener noreferrer"
+        underline={'always'}
+        color="inherit"
+      >
+        {'Material-UI'}
+      </Link>
     </div>
   )
 }
