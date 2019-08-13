a9ce2216ec8f269b648b7c123d6e1236a244c4e0
mnogueron <matthieu.nogueron@live.fr>
2019-08-13 21:39:41 +0200

    Override Workbox configuration to support new blacklisted url

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,16 @@ const imageminMozjpeg = require('imagemin-mozjpeg')
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
 
+  if (env === 'production') {
+    // remove GenerateSW plugin to add our own configuration
+    config.plugins.map(plugin => {
+      if (plugin.constructor.name === 'GenerateSW') {
+        plugin.config.navigateFallbackBlacklist.push(new RegExp('^.*(/react-easy-panzoom/?).*$'))
+      }
+      return plugin
+    })
+  }
+
   config.plugins.push(new ImageminPlugin({
     disable: env !== 'production', // Disable during development
     jpegtran: null,
